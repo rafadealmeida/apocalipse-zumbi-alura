bbe69a78d1595ff3e5f29b8aa8ce54181bc39386
Rafael Soares de Almeida <93219825+rafadealmeida@users.noreply.github.com>
2021-11-12 18:39:18 -0300

    teste

---
+++ Assets/scriptsJogoZumbi/Bala.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{   
+    public  float Velocidade = 20;
+    // Start is called before the first frame update
+   
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //movimento da bala.
+        GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + transform.forward);
+    }
+}
